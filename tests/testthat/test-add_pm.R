71ca88da0f17e8c42a1f37dacf7463f9c2eeda00
Erika Rasnick <45949235+erikarasnick@users.noreply.github.com>
2021-09-09 11:09:01 -0400

    aws credentials not needed anymore (#15)

--- tests/testthat/test-add_pm.R
+++ tests/testthat/test-add_pm.R
@@ -16,14 +16,6 @@ input_data_h3 <- function() {
     dplyr::mutate(dplyr::across(c(start_date, end_date), as.Date))
 }
 
-test_that("add_pm errors without credentials", {
-  expect_error(
-    withr::with_envvar(new = c(
-      "AWS_ACCESS_KEY_ID" = NA,
-      "AWS_SECRET_ACCESS_KEY" = NA
-    ), {
-      add_pm(input_data()) }))})
-
 test_that("add_pm works", {
   expect_equal(
     add_pm(input_data()),
